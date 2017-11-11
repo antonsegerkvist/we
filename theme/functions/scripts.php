7d5200c9f9e1dcc66eed2066d99c6ccf367abf04
Anton Segerkvist <anton.stefan.roine.segerkvist@gmail.com>
2017-11-11 11:28:29 +0100

    Updated package.json and fixed script hook.

--- theme/functions/scripts.php
+++ theme/functions/scripts.php
@@ -1,6 +1,6 @@
 <?php
 function we_scripts () {
-  wp_enqueue_style ('we_js_bundle', get_theme_file_uri('/assets/js/app.bundle.js'), array(), '1.0');
+  wp_enqueue_script ('we_js_bundle', get_theme_file_uri('/assets/js/app.bundle.js'), array(), '1.0');
 }
 add_action ('wp_enqueue_scripts', 'we_scripts');
 ?>
