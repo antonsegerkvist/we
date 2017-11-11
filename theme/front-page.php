edb3fc26a0bbb04f8374ccedb7a96d525807a4fb
Anton Segerkvist <anton.stefan.roine.segerkvist@gmail.com>
2017-11-11 09:25:56 +0100

    Fixing front-page errors.

--- theme/front-page.php
+++ theme/front-page.php
@@ -1,3 +1,15 @@
 <?php get_header(); ?>
 
+<section class="front-page-main">
+</section>
+
+<section class="front-page-about">
+</section>
+
+<section class="front-page-forum">
+</section>
+
+<section class="front-page-credits">
+</section>
+
 <?php get_footer(); ?>
