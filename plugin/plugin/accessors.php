1824c2e82a3f38a3443525c703ef8b6f58100d4e
Anton Segerkvist <anton.stefan.roine.segerkvist@gmail.com>
2017-11-17 22:46:37 +0100

    Work on registration function.

--- plugin/plugin/accessors.php
+++ plugin/plugin/accessors.php
@@ -8,19 +8,34 @@ defined ('ABSPATH') or die ('Direct access not allowed');
 class We_Accessors {
 
   /**
-   * Initialization method.
-   */
-
-  public static function run () {
-    add_action();
-  }
-
-  /**
    * User registration endpoint.
    */
 
   public static function endpoint_register_user () {
+    $user_login = $_POST['username'];
+    $user_email = $_POST['email'];
+    $error = register_new_user($user_login, $user_email);
 
+    if (!is_wp_error($error)) {
+      echo json_encode(array(
+        'success' => true
+      ));
+    } else {
+      echo json_encode(array(
+        'success' => false,
+        'error' => $error->errors
+      ));
+    }
+
+    wp_die();
+  }
+
+  /**
+   * Initialization method.
+   */
+
+  public static function run () {
+    add_action('wp_ajax_nopriv_endpoint_register_user', array('We_Accessors', 'endpoint_register_user'));
   }
 
 }
